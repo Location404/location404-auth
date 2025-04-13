6c543ebd31f27d5bdcb14eae65aece6e318b79af
ryanbromati <ryan.bromati@hotmail.com>
2025-04-13 19:59:15 -0300

    feat: adicionar classes ValueObject e EmailAddress para representação de valores no domínio

---
+++ src/UserIdentity.Domain/Common/ValueObject.cs
@@ -0,0 +1,6 @@
+namespace UserIdentity.Domain.Common;
+
+public abstract class ValueObject
+{
+    public string Value = null!;
+}
---
+++ src/UserIdentity.Domain/ValueObject/EmailAddress.cs
@@ -0,0 +1,20 @@
+using System.Text.RegularExpressions;
+
+namespace UserIdentity.Domain.ValueObject;
+
+public sealed partial class EmailAddress : Common.ValueObject
+{
+    public EmailAddress(string emailAddress)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(emailAddress);
+        if (EmailAddressPattern().IsMatch(emailAddress))
+        {
+            throw new ArgumentException(nameof(EmailAddress));
+        }
+
+        Value = emailAddress;
+    }
+
+    [GeneratedRegex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", RegexOptions.IgnoreCase | RegexOptions.Compiled, "")]
+    private static partial Regex EmailAddressPattern();
+}
