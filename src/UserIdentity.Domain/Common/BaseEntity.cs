743b506ca370064abba131e32da98c1d7c4586b5
ryanbromati <ryan.bromati@hotmail.com>
2025-04-16 19:44:04 -0300

    feat: refatorar entidade User para melhorar a estrutura

--- src/UserIdentity.Domain/Common/BaseEntity.cs
+++ src/UserIdentity.Domain/Common/BaseEntity.cs
@@ -2,19 +2,5 @@ namespace UserIdentity.Domain.Common;
 
 public abstract class BaseEntity
 {
-    public Guid Id { get; private set; }
-    public DateTime CreatedAt { get; private set; }
-    public DateTime UpdatedAt { get; private set; }
-
-    protected BaseEntity()
-    {
-        Id = Guid.CreateVersion7();
-        CreatedAt = DateTime.UtcNow;
-        UpdatedAt = DateTime.UtcNow;
-    }
-
-    public void Update()
-    {
-        UpdatedAt = DateTime.UtcNow;
-    }
+    public Guid Id { get; private set; } = Guid.CreateVersion7();
 }
