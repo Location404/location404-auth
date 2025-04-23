080d3264247f92434e03fe654693fc424882b19e
ryan <ryan.bromati@hotmail.com>
2025-04-23 20:14:36 -0300

    feat: refatorar entidade usuario para suportar password e oauth

---
+++ src/UserIdentity.Domain/Entities/LoginPassword.cs
@@ -0,0 +1,20 @@
+using UserIdentity.Domain.Common;
+
+namespace UserIdentity.Domain.Entities;
+
+public class LoginPassword(string passwordHash, string passwordSalt, string refreshToken, DateTime refreshTokenExpirationDate) : BaseEntity
+{
+    public virtual UserApplication UserApplication { get; private set; } = null!;
+
+    public string PasswordHash { get; private set; } = passwordHash;
+    public string PasswordSalt { get; private set; } = passwordSalt;
+
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+    public DateTime LastLoginAt { get; private set; } = DateTime.UtcNow;
+
+    public string RefreshToken { get; private set; } = refreshToken;
+    public DateTime RefreshTokenExpirationDate { get; private set; } = refreshTokenExpirationDate;
+
+    public bool IsExpired() => DateTime.UtcNow > RefreshTokenExpirationDate;
+    public void UpdateLastLogin() => LastLoginAt = DateTime.UtcNow;
+}
