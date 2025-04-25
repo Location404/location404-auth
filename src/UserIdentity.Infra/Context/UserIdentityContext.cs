58a26a6a484bb5f6b43f60361662000479fcb768
ryan <ryan.bromati@hotmail.com>
2025-04-25 20:52:40 -0300

    feat: adicionar classe de injeção de dependência para UserIdentity

--- src/UserIdentity.Infra/Context/UserIdentityContext.cs
+++ src/UserIdentity.Infra/Context/UserIdentityContext.cs
@@ -6,6 +6,8 @@ namespace UserIdentity.Infra.Context;
 public class UserIdentityContext(DbContextOptions<DbContext> options) : DbContext(options)
 {
     public DbSet<UserApplication> Users { get; set; }
+    public DbSet<OAuthAuthentication> OAuthAuthentications { get; set; }
+    public DbSet<PasswordAuthentication> PasswordAuthentications { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
