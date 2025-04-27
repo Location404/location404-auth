27e609d700a66a51392bfe5a6befd8fd30b2f2a9
ryanbromati <ryan.bromati@hotmail.com>
2025-04-27 07:10:49 -0300

    refactor: renomear DbSet de Users para UserApplication e remover autenticações OAuth e senha

--- src/UserIdentity.Infra/Context/UserIdentityContext.cs
+++ src/UserIdentity.Infra/Context/UserIdentityContext.cs
@@ -1,13 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+
 using UserIdentity.Domain.Entities;
 
 namespace UserIdentity.Infra.Context;
 
 public class UserIdentityContext(DbContextOptions<DbContext> options) : DbContext(options)
 {
-    public DbSet<UserApplication> Users { get; set; }
-    public DbSet<OAuthAuthentication> OAuthAuthentications { get; set; }
-    public DbSet<PasswordAuthentication> PasswordAuthentications { get; set; }
+    public DbSet<UserApplication> UserApplication { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
