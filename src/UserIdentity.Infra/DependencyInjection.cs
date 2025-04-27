58c46fe492094835262036aea71c8e89e14f7a13
ryanbromati <ryan.bromati@hotmail.com>
2025-04-27 18:09:15 -0300

    feat: adicionar serviços de injeção de dependência para gerenciamento de tokens e persistência de dados

--- src/UserIdentity.Infra/DependencyInjection.cs
+++ src/UserIdentity.Infra/DependencyInjection.cs
@@ -1,5 +1,13 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+using UserIdentity.Application.Common.Interfaces;
+using UserIdentity.Application.Features.Authentication.Interfaces;
+using UserIdentity.Application.Features.UserManagement;
+using UserIdentity.Infra.Context;
+using UserIdentity.Infra.Persistence;
+using UserIdentity.Infra.Services;
 
 namespace UserIdentity.Infra;
 
@@ -7,6 +15,22 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddUserIdentityInfra(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        services.AddSingleton<IPasswordService, PasswordService>();
+        services.AddSingleton<ITokenService, JwtTokenService>();
+
+        services.AddDbContext<UserIdentityContext>(op =>
+        {
+            op.UseNpgsql(configuration.GetConnectionString("UserIdentityDb"));
+
+#if DEBUG
+            op.EnableSensitiveDataLogging();
+            op.EnableDetailedErrors();
+#endif
+        });
+
         return services;
     }
 }
