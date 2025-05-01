2eac86027fdd854284afa556e021287f192229ac
ryanbromati <ryan.bromati@hotmail.com>
2025-05-01 17:10:48 -0300

    feat: aumentar o tamanho dos campos PasswordHash, PasswordSalt e RefreshToken para 256 caracteres

--- src/UserIdentity.Infra/Migrations/20250501200951_FixTokensLength.Designer.cs
+++ src/UserIdentity.Infra/Migrations/20250501200951_FixTokensLength.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UserIdentity.Infra.Context;
@@ -11,9 +12,11 @@ using UserIdentity.Infra.Context;
 namespace UserIdentity.Infra.Migrations
 {
     [DbContext(typeof(UserIdentityContext))]
-    partial class UserIdentityContextModelSnapshot : ModelSnapshot
+    [Migration("20250501200951_FixTokensLength")]
+    partial class FixTokensLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
