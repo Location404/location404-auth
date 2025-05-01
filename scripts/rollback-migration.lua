1b73fbfe7c8d2985eb9a84aceb6c1861d9c1065c
ryanbromati <ryan.bromati@hotmail.com>
2025-05-01 16:59:22 -0300

    feat: adicionar script para reverter migrações no EF Core

---
+++ scripts/rollback-migration.lua
@@ -0,0 +1,8 @@
+-- rollback-migration.lua
+local args = {...}
+local target = args[1] or "0"
+
+local command = string.format("dotnet ef database update %s --project ../src/UserIdentity.Infra --startup-project ../src/UserIdentity.API", target)
+print("Executando:", command)
+
+os.execute(command)
