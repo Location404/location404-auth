4323a115d840ad4dcf974fe9b5c1e4206a8b07af
ryanbromati <ryan.bromati@hotmail.com>
2025-05-01 17:00:00 -0300

    feat: adicionar migrações iniciais e índices únicos para a tabela UserApplication

---
+++ src/UserIdentity.Infra/Migrations/20250501195822_CreateIndex.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserIdentity.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "idx_user_application_email_address",
+                table: "user_application",
+                column: "email_address",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "idx_user_application_google_id",
+                table: "user_application",
+                column: "google_id",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "idx_user_application_username",
+                table: "user_application",
+                column: "username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "idx_user_application_email_address",
+                table: "user_application");
+
+            migrationBuilder.DropIndex(
+                name: "idx_user_application_google_id",
+                table: "user_application");
+
+            migrationBuilder.DropIndex(
+                name: "idx_user_application_username",
+                table: "user_application");
+        }
+    }
+}
