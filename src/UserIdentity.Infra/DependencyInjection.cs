59522f9f92b496fe154e40e7dad2d1ae08e265f3
ryan <ryan.bromati@hotmail.com>
2025-05-09 20:29:58 -0300

    feat: adicionar suporte à autenticação JWT e configuração de serviços relacionados

--- src/UserIdentity.Infra/DependencyInjection.cs
+++ src/UserIdentity.Infra/DependencyInjection.cs
@@ -1,9 +1,13 @@
+using System.Text;
 using MediatR;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 using UserIdentity.Application.Common.Interfaces;
 using UserIdentity.Application.Features.Authentication.Interfaces;
@@ -20,6 +24,59 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddUserIdentityInfra(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddUserIdentitySettings(configuration);
+        services.AddUserIdentityDbContext(configuration);
+        services.AddUserIdentityServices();
+        services.AddUserIdentityAuthentication(configuration);
+
+        return services;
+    }
+
+    public static IServiceCollection AddUserIdentityAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        var jwtSettings = configuration.GetSection(JwtSettings.SectionName).Get<JwtSettings>();
+        ArgumentNullException.ThrowIfNull(jwtSettings, nameof(jwtSettings));
+
+        services.AddAuthorization();
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.SaveToken = true;
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtSettings!.Issuer,
+                ValidAudience = jwtSettings.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey)),
+                RequireExpirationTime = true,
+            };
+
+            options.Events = new JwtBearerEvents
+            {
+                OnAuthenticationFailed = context =>
+                {
+                    if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                    {
+                        context.Response.Headers.Append("Token-Expired", "true");
+                    }
+                    return Task.CompletedTask;
+                }
+            };
+        });
+
+        return services;
+    }
+
+
+    public static IServiceCollection AddUserIdentityDbContext(this IServiceCollection services, IConfiguration configuration)
+    {
         services.AddDbContext<UserIdentityContext>(op =>
         {
 #if DEBUG
@@ -31,13 +88,23 @@ public static class DependencyInjection
 #endif
         });
 
+        return services;
+    }
+
+    public static IServiceCollection AddUserIdentityServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IPasswordService, PasswordService>();
+        services.AddSingleton<ITokenService, JwtTokenService>();
+
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(RegisterUserCommand).Assembly));
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-        services.AddSingleton<IPasswordService, PasswordService>();
-        services.AddSingleton<ITokenService, JwtTokenService>();
+        return services;
+    }
 
+    public static IServiceCollection AddUserIdentitySettings(this IServiceCollection services, IConfiguration configuration)
+    {
         services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
 
         return services;
