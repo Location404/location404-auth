8f574af3c8adb695cfda94f51eb5c91589a6203b
ryanbromati <ryan.bromati@hotmail.com>
2025-05-15 21:10:04 -0300

    feat: adicionar suporte a configuração de login externo com Google e ajustar estrutura de dados

--- src/UserIdentity.Infra/Common/ExternalLoginBase.cs
+++ src/UserIdentity.Infra/Common/ExternalLoginBase.cs
@@ -1,9 +1,9 @@
 namespace UserIdentity.Infra.Common;
 
-public abstract record ExternalLoginSettingsBase(string ClientId, string ClientSecret, string CallbackPath)
+public abstract record ExternalLoginSettingsBase(string ClientId, string ClientSecret, string ClientCallbackUrl)
 {
-    public static string SectionName = "ExternalLogin__";
+    protected static string ExternalLoginSectionName = "ExternalLogin:";
     public string ClientId { get; private set; } = ClientId;
     public string ClientSecret { get; private set; } = ClientSecret;
-    public string CallbackPath { get; private set; } = CallbackPath;
+    public string ClientCallbackUrl { get; private set; } = ClientCallbackUrl;
 }
