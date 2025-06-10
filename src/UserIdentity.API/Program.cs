688f4d14c855bcc571b839f32a62252e51477524
ryan <ryan.bromati@hotmail.com>
2025-06-10 10:01:45 -0300

    feat: adicionar suporte à observabilidade com configuração no appsettings e integração no Program.cs

--- src/UserIdentity.API/Program.cs
+++ src/UserIdentity.API/Program.cs
@@ -1,10 +1,13 @@
 using UserIdentity.API.Endpoints;
 using UserIdentity.Infra;
+using ObservabilitySdk.Observability;
+using ObservabilitySdk.AspNet;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddObservability(builder.Configuration, builder.Environment);
 builder.Services.AddUserIdentityInfra(builder.Configuration);
 
 var app = builder.Build();
@@ -14,6 +17,7 @@ app.MapOpenApi();
 
 app.MapUserEndpoints();
 app.MapAuthenticationEndpoints();
+app.UseObservability();
 
 app.UseExceptionHandler("/error");
 
