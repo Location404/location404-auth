fdfcf95f0311db49218b7392fb96fd381ff7ee12
ryanbromati <ryan.bromati@hotmail.com>
2025-08-07 21:37:18 -0300

    refactor:  User Identity Service: Remove obsolete migrations, repositories, and services; introduce new API project structure and domain entities
    - Deleted outdated migration files related to token length and external login user.
    - Removed UnitOfWork and UserRepository implementations.
    - Eliminated JwtTokenService and PasswordService classes.
    - Created new API project with basic setup and launch settings.
    - Added new domain entities: User and ExternalLogin, along with value objects for EmailAddress and Level.
    - Established project structure for UserIdentityService with necessary configurations.

--- src/UserIdentity.API/Endpoints/UserEndpoint.cs
+++
@@ -1,9 +0,0 @@
-namespace UserIdentity.API.Endpoints;
-
-public static class UserEndpoint
-{
-    public static void MapUserEndpoints(this IEndpointRouteBuilder endpoint)
-    {
-        var group = endpoint.MapGroup("/api/v1/user").WithTags("User");
-    }
-}
