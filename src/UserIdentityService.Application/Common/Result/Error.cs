04c14ba9454d62f23dd11430f0cfdabe5afbff2e
ryanbromati <ryan.bromati@hotmail.com>
2025-08-07 22:16:11 -0300

    feat: adicionar classes Result e Error para gerenciamento de resultados e erros

---
+++ src/UserIdentityService.Application/Common/Result/Error.cs
@@ -0,0 +1,38 @@
+namespace UserIdentityService.Application.Common.Result;
+
+public enum ErrorType
+{
+    /// <summary>
+    /// Um erro genérico de falha.
+    /// </summary>
+    Failure,
+
+    /// <summary>
+    /// Um erro de validação (ex: input inválido).
+    /// </summary>
+    Validation,
+
+    /// <summary>
+    /// Um recurso não foi encontrado.
+    /// </summary>
+    NotFound,
+
+    /// <summary>
+    /// Houve um conflito de estado (ex: tentar criar um recurso que já existe).
+    /// </summary>
+    Conflict,
+
+    /// <summary>
+    /// O requisitante não tem autorização.
+    /// </summary>
+    Unauthorized,
+
+    /// <summary>
+    /// O requisitante não está autenticado.
+    /// </summary>
+    UnAuthenticated
+}
+public record Error(string Code, string Message, ErrorType Type = ErrorType.Failure)
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+}
