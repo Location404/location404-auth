442f2d8e8749cc02a344fc40c7308a445f50780f
ryanbromati <ryan.bromati@hotmail.com>
2025-08-09 19:05:59 -0300

    feat: alterar a estrutura do perfil do usuário para armazenar imagem como byte array

--- src/UserIdentityService.Infrastructure/Migrations/20250809214829_ImageUrlBTEA.Designer.cs
+++ src/UserIdentityService.Infrastructure/Migrations/20250809214829_ImageUrlBTEA.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UserIdentityService.Infrastructure.Context;
@@ -11,9 +12,11 @@ using UserIdentityService.Infrastructure.Context;
 namespace UserIdentityService.Infrastructure.Migrations
 {
     [DbContext(typeof(UserIdentityDbContext))]
-    partial class UserIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250809214829_ImageUrlBTEA")]
+    partial class ImageUrlBTEA
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
