018fc6171ffa556b39c021f6a57833e060f7850e
ryanbromati <ryan.bromati@hotmail.com>
2025-08-12 23:05:28 -0300

    feat: adicionar suporte ao LiteBus para gerenciamento de comandos

--- src/UserIdentityService.Infrastructure/DependencyInjection.cs
+++ src/UserIdentityService.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,11 @@
+using LiteBus.Commands.Extensions.MicrosoftDependencyInjection;
+using LiteBus.Messaging.Extensions.MicrosoftDependencyInjection;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+using UserIdentityService.Application.Features.UserManagement.Commands.CreateUserWithPasswordCommand;
 
 namespace UserIdentityService.Infrastructure;
 
@@ -8,15 +13,20 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddUserIdentityInfra(this IServiceCollection services, IConfiguration configuration)
     {
-        // Add DbContext
         services.AddDbContext<Context.UserIdentityDbContext>(options =>
         {
             var connectionString = configuration.GetConnectionString("UserIdentityDatabase");
             options.UseNpgsql(connectionString);
 
-            // Enable sensitive data logging
             options.EnableSensitiveDataLogging();
             options.EnableDetailedErrors();
+
+        });
+        
+        services.AddLiteBus(liteBus =>
+        {
+            liteBus.AddCommandModule(module => module.RegisterFromAssembly(typeof(CreateUserWithPasswordHandler).Assembly));
+            // liteBus.AddQueryModule(module => module.RegisterFromAssembly(typeof(CreateUserWithExternalProviderHandler).Assembly));
         });
 
         return services;
