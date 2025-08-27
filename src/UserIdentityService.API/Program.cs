a8b12f00e0ba8e12f2ff072fd907ff86a396eada
ryanbromati <ryan.bromati@hotmail.com>
2025-08-26 22:13:19 -0300

    :)

--- src/UserIdentityService.API/Program.cs
+++ src/UserIdentityService.API/Program.cs
@@ -8,14 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddUserIdentityInfra(builder.Configuration);
-builder.Services.AddObservability(builder.Configuration);
 
 var app = builder.Build();
 
 app.MapOpenApi();
 app.MapUserManagementEndpoints();
 
-app.UseObservability();
 app.UseHttpsRedirection();
 app.MapScalarApiReference();
 
