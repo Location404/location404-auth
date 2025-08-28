ee796bfa99fb627cfe7efc4b8b3a3fce37356d51
ryanbromati <ryan.bromati@hotmail.com>
2025-08-28 16:27:13 -0300

    feat: adicionando e configurando servico compartilhado de observabilidade

--- src/UserIdentityService.API/Program.cs
+++ src/UserIdentityService.API/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddUserIdentityInfra(builder.Configuration);
+builder.Services.AddOpenTelemetryObservability(builder.Configuration);
 
 var app = builder.Build();
 
