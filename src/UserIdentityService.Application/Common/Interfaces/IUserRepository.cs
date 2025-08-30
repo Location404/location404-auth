a72e8a411192e341ce94946f4a4b34881c1a5b01
ryanbromati <ryan.bromati@hotmail.com>
2025-08-30 00:24:41 -0300

    feat: implementar serviços de autenticação e gerenciamento de tokens

--- src/UserIdentityService.Application/Common/Interfaces/IUserRepository.cs
+++ src/UserIdentityService.Application/Common/Interfaces/IUserRepository.cs
@@ -5,9 +5,19 @@ namespace UserIdentityService.Application.Common.Interfaces;
 public interface IUserRepository : IDisposable
 {
     Task<User?> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken = default);
+    Task<User?> GetUserByEmailAsync(string email, CancellationToken cancellationToken = default);
     Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken = default);
     Task<bool> ExistsByUsernameAsync(string username, CancellationToken cancellationToken = default);
     Task AddUserAsync(User user, CancellationToken cancellationToken = default);
     bool UpdateUserAsync(User user, CancellationToken cancellationToken = default);
     Task<bool> DeleteUserAsync(Guid userId, CancellationToken cancellationToken = default);
+
+
+    #region [RefreshToken]
+
+    void AddRefreshToken(RefreshToken token, CancellationToken cancellationToken);
+    Task<RefreshToken?> GetByTokenAsync(Guid userId, string token, CancellationToken cancellationToken);
+    Task RevokeAllByUserAsync(Guid userId, CancellationToken cancellationToken);
+
+    #endregion
 }
--- src/UserIdentityService.Infrastructure/Services/UserRepository.cs
+++ src/UserIdentityService.Infrastructure/Services/UserRepository.cs
@@ -38,19 +38,54 @@ public class UserRepository(UserIdentityDbContext context) : IUserRepository
         return entry.State == EntityState.Modified;
     }
 
+    public async Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken = default)
+    {
+        return await _context.Users.AnyAsync(u => u.Email == email, cancellationToken);
+    }
+
+    public async Task<bool> ExistsByUserIdAsync(Guid userId, CancellationToken cancellationToken = default)
+    {
+        return await _context.Users.AnyAsync(u => u.Id == userId, cancellationToken);
+    }
+
+    public async Task<bool> ExistsByUsernameAsync(string username, CancellationToken cancellationToken = default)
+    {
+        return await _context.Users.AnyAsync(u => u.Username == username, cancellationToken);
+    }
+
+    public async Task<User?> GetUserByEmailAsync(string email, CancellationToken cancellationToken = default)
+    {
+        return await _context.Users.SingleOrDefaultAsync(u => u.Email == email, cancellationToken);
+    }
+
+    # region [RefreshTokens]
+
+    public void AddRefreshToken(RefreshToken token, CancellationToken cancellationToken)
+    {
+        _context.RefreshTokens.Add(token);
+    }
+
+    public async Task<RefreshToken?> GetByTokenAsync(Guid userId, string token, CancellationToken cancellationToken)
+    {
+        return await _context.RefreshTokens
+            .Where(rt => rt.UserId == userId && rt.Token == token)
+            .SingleOrDefaultAsync(cancellationToken);
+    }
+
+    public async Task RevokeAllByUserAsync(Guid userId, CancellationToken cancellationToken)
+    {
+        var refreshTokens = await _context.RefreshTokens
+            .Where(rt => rt.UserId == userId)
+            .ToListAsync(cancellationToken);
+
+        _context.RefreshTokens.RemoveRange(refreshTokens);
+    }
+
     public void Dispose()
     {
         _context.Dispose();
         GC.SuppressFinalize(this);
     }
 
-    public Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken = default)
-    {
-        return _context.Users.AnyAsync(u => u.Email == email, cancellationToken);
-    }
-
-    public Task<bool> ExistsByUsernameAsync(string username, CancellationToken cancellationToken = default)
-    {
-        return _context.Users.AnyAsync(u => u.Username == username, cancellationToken);
-    }
+    #endregion
 }
